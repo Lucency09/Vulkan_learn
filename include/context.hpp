99d3f41d7821c7b57b6c36e2d859dc36682eda07
Lucency09 <gzgypzy@foxmail.com>
2023-09-18 00:11:21 +0800

    物理设备

--- include/context.hpp
+++ include/context.hpp
@@ -2,6 +2,7 @@
 
 #include "vulkan/vulkan.hpp"
 #include <iostream>
+#include <optional>
 
 namespace toy2d
 {
@@ -15,9 +16,25 @@ namespace toy2d
 		~Context();
 		
 	private:
+		struct QueueFamliyIndices final
+		{
+			std::optional<uint32_t> graphicsQueue;
+		};
+
 		Context();
-		static Context* instance_;
-		vk::Instance instance = nullptr;
+		static Context* instance_;//存储自身
+		vk::Instance instance = nullptr;//存储Vulkan句柄
+		vk::PhysicalDevice phyDevice;//存储物理设备
+		vk::Device device;//存储逻辑设备
+		vk::Queue graphcisQueue;//图像命令列表
+		QueueFamliyIndices queueFamilyIndices;//存储命令队列
+
+		void createInstance();//创建Vulkan句柄
+		void pickupPhyiscalDevice();//选择物理设备
+		void creatDevice();//创建逻辑设备
+		void getQueues();//设置graphcisQueue值
+
+		void queryQueueFamilyIndices();//查询支持的命令队列
 		
 	};
 }
--- src/context.cpp
+++ src/context.cpp
@@ -27,8 +27,16 @@ namespace toy2d
 
 	Context::Context()
 	{
-		vk::InstanceCreateInfo createInfo;
+		createInstance();
+		pickupPhyiscalDevice();
+		queryQueueFamilyIndices();
+		creatDevice();
+		getQueues();
+	}
 
+	void Context::createInstance()
+	{
+		vk::InstanceCreateInfo createInfo;
 		/*查询所有支持的层，暂时不用
 		auto layers = vk::enumerateInstanceLayerProperties();
 		for (auto& layer : layers)
@@ -36,7 +44,6 @@ namespace toy2d
 			std::cout << layer.layerName << std::endl;
 		}
 		*/
-
 		vk::ApplicationInfo appInfo;
 		appInfo.setApiVersion(VK_API_VERSION_1_3);
 		createInfo.setPApplicationInfo(&appInfo);
@@ -46,8 +53,60 @@ namespace toy2d
 
 		instance = vk::createInstance(createInfo);//创建Vulkan句柄
 	}
+
+	void Context::pickupPhyiscalDevice()
+	{ 
+		auto devices = instance.enumeratePhysicalDevices();//获取设备列表
+		//遍历所有设备
+		for (auto& device : devices)
+		{
+			auto feature = device.getFeatures();//设备支持的特性
+			std::cout << device.getProperties().deviceName;//打印设备名称
+			std::cout << "  " << device.getProperties().deviceID;//设备ID
+			std::cout << "  " << device.getProperties().apiVersion << std::endl;//api版本
+		}
+		//
+		phyDevice = devices[0];//只有一张显卡，故设置为第一张
+		
+	}
+
+	void Context::creatDevice()
+	{
+		vk::DeviceCreateInfo devicecreateInfo;
+		//createInfo.setPEnabledLayerNames();//继承creatInstance步骤中的信息，但可设置逻辑设备独有的验证
+		vk::DeviceQueueCreateInfo queueCreateInfo;//命令缓冲队列？
+		float priorities = 1.0;//优先级，最高1最低0
+		queueCreateInfo.setPQueuePriorities(&priorities)
+			.setQueueCount(1)//数量
+			.setQueueFamilyIndex(queueFamilyIndices.graphicsQueue.value());//类型
+
+		devicecreateInfo.setQueueCreateInfos(queueCreateInfo);
+		device = phyDevice.createDevice(devicecreateInfo);
+	}
+
+	void Context::queryQueueFamilyIndices()
+	{
+		auto properties = phyDevice.getQueueFamilyProperties();//向逻辑设备查询
+		for (int i = 0; i < properties.size(); i++)
+		{
+			const auto& property = properties[i];
+			if (property.queueFlags | vk::QueueFlagBits::eGraphics)
+			{
+				queueFamilyIndices.graphicsQueue = i;
+				break;
+			}
+		}
+
+	}
+
+	void Context::getQueues()
+	{
+		graphcisQueue = device.getQueue(queueFamilyIndices.graphicsQueue.value(), 0);
+	}
+
 	Context::~Context()
 	{
+		device.destroy();
 		instance.destroy();
 	}
 
