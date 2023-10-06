d3988be666f7df0cef89e50e4da5a22380c5e209
Lucency09 <cngzgypzy@outlook.com>
2023-10-06 17:28:48 +0800

    交换链

--- include/swapchain.hpp
+++ include/swapchain.hpp
@@ -2,6 +2,8 @@
 
 #include "vulkan/vulkan.hpp"
 
+#include "context.hpp"
+
 namespace toy2d
 {
 	class Swapchain final
@@ -10,6 +12,13 @@ namespace toy2d
 		Swapchain();
 		~Swapchain();
 	private:
+		struct SwapchainInfo {
+			vk::Extent2D imageExtent;//图像大小
+			uint32_t imageCount;//图像数量
+			vk::Format format;//图像的颜色属性
+		};
+		SwapchainInfo info;
 		vk::SwapchainKHR swapchain;
+		void queryInfo();
 	};
 }
--- src/swapchain.cpp
+++ src/swapchain.cpp
@@ -4,12 +4,19 @@ namespace toy2d
 {
 	Swapchain::Swapchain()
 	{
+		queryInfo();
 		vk::SwapchainCreateInfoKHR createInfo;
 		createInfo.setClipped(true)//当画布尺寸大于窗口尺寸时是否裁剪
-					.setImageArrayLayers(1)//设置交换链视图数量(屏幕数量？)
-					
+			.setImageArrayLayers(1)//设置交换链视图数量(屏幕数量？)
+			.setImageUsage(vk::ImageUsageFlagBits::eColorAttachment)//颜色附件？运行GUP绘制彩色像素点？
+			.setCompositeAlpha(vk::CompositeAlphaFlagBitsKHR::eOpaque)//设置色彩不透明(不与窗口外？其他色彩混合)
+			.setSurface(Context::GetInstance().get_surface());
 	}
 	Swapchain::~Swapchain()
 	{
+	}
+	void Swapchain::queryInfo()
+	{
+		Context::GetInstance().
 	}
 }
